c4020f2d9aef612ca77769e6f9d78dbd0b05a2f9
SergiyMogiley1983 <sergiymogiley@gmail.com>
2025-11-15 13:09:32 +0200

    Update 7_1_greetings.py

--- 7_1_greetings.py
+++ 7_1_greetings.py
@@ -1,5 +1,5 @@
 def say_hi(name, age):
-    return str("Hi. My name is " + name + " and I'm " + str(age) + " years old")
+    return "Hi. My name is " + name + " and I'm " + str(age) + " years old"
 assert say_hi("Alex", 32) == "Hi. My name is Alex and I'm 32 years old", 'Test1'
 assert say_hi("Frank", 68) == "Hi. My name is Frank and I'm 68 years old", 'Test2'
 #assert say_hi("Vasya", 101) == "Hi. My name is Vasya and I'm 101 years old", 'Test3'
